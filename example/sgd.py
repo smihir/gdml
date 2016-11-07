4d5ec8e680738a5f38222456d5d507a784189893
Felipe Gutierrez <fgutierrez3@wisc.edu>
2016-11-07 03:11:16 -0600

    add SGD for Multiple Linear Regression with PS

--- example/sgd.py
+++ example/sgd.py
@@ -1,50 +1,84 @@
 import find_mxnet
 import mxnet as mx
+import inspect
+# from numpy import *
+import numpy as np
+import matplotlib
+import os, sys,time
 
-from numpy import *
+def ComputeMSE(y,y_p):
+	return ((y - y_p) ** 2).mean(axis=0)
 
-# y = mx + b
-# m is slope, b is y-intercept
-def compute_error_for_line_given_points(b, m, points):
-	totalError = 0
-	for i in range(0, len(points)):
-	    x = points[i, 0]
-	    y = points[i, 1]
-	    totalError += (y - (m * x + b)) ** 2
-	return totalError / float(len(points))
+def ComputeCostF(y,y_p):
+	numElements = float(y.size)
+	return (1/(2*numElements)) * ComputeMSE
 
-def step_gradient(b_current, m_current, points, learningRate):
-	b_gradient = 0
-	m_gradient = 0
-	N = float(len(points))
-	for i in range(0, len(points)):
-	    x = points[i, 0]
-	    y = points[i, 1]
-	    b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
-	    m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
-	new_b = b_current - (learningRate * b_gradient)
-	new_m = m_current - (learningRate * m_gradient)
-	return [new_b, new_m]
+def ComputeGrad(y,y_p):
+	sumOfDifference = np.sum(y_p - y)
+	numElements = float(y.size)
+	grad = (1/numElements)*sumOfDifference
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
-	b = starting_b
-	m = starting_m
-	for i in range(num_iterations):
-		if (i % 50 == 0):
-			print "Iteration {0}, m = {1}, b = {2}".format(i,m,b)
-		b, m = step_gradient(b, m, array(points), learning_rate)
-	return [b, m]
+	return grad
 
-def run():
-	points = genfromtxt("data/sample_data.csv", delimiter=",")
-	learning_rate = 0.0001
-	initial_b = 0 # initial y-intercept guess
-	initial_m = 0 # initial slope guess
-	num_iterations = 1000
-	print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
-	print "Running..."
-	[b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-	print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+# kv = mx.kv.create('dist_sync')
+kv = mx.kv.create('local')
+data_path = 'data/sample_data';
+nu = 0.0001
 
-if __name__ == '__main__':
-    run()
+rank = kv.rank
+print "rank = {0}".format(rank)
+
+
+points = np.genfromtxt(data_path+str(rank)+".csv", delimiter=",")
+(numRows,numCols) = points.shape
+print "numRows = {0}".format(numRows)
+print "numCols = {0}".format(numCols)
+
+# x = np.array([points[0:,0:], np.ones(numRows).transpose()])
+# x = np.transpose(x)
+x = np.concatenate((points[0:,0:1],points[0:,0:1],points[0:,0:1]),1)
+y = points[0:,1]
+
+(numPoints,dim) = x.shape  
+# print x[:,1:2]
+
+# Init weight vectors
+w = np.zeros(dim)
+w_id = 1
+w_local = mx.nd.array(w)
+kv.push(w_id, w_local)
+
+batchSize = 1
+currBatch = 0
+for i in range(numPoints):	
+	# Read mini-batch
+	currX = x[currBatch * batchSize:(currBatch+1) * batchSize,:]
+	currY = y[currBatch * batchSize:(currBatch+1) * batchSize]
+	
+	# Read weight vector
+	kv.pull(w_id, out = w_local) 
+	# Compute hypothesis output
+	w_local_np = w_local.asnumpy()
+	y_p = np.matmul(currX,w_local_np)
+	# Compute gradient
+	grad = ComputeGrad(currY,y_p)
+
+	# Update weight vector
+	w_local = mx.nd.array(w_local_np - nu * grad * x[i])
+	kv.push(w_id,w_local)
+
+
+
+# kv.pull(w_id,out = w_local)
+# y_p = np.matmul(x,w_local.asnumpy())
+# print ComputeMSE(y,y_p)
+	
+
+
+
+
+
+
+
+
+
