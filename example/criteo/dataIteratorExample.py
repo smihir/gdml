ff9e34c822f9e489649a2233f0d1abd25c49c8bf
Mihir Shete <mihirsht@gmail.com>
2016-11-18 00:17:53 +0000

    first attempts in criteo training

--- example/criteo/dataIteratorExample.py
+++ example/criteo/dataIteratorExample.py
@@ -6,15 +6,16 @@ import mxnet as mx
 import CriteoParser as parser
 
 filepath = 'day_0_10000.txt'
-
-# batchSize = 100
-# trainSize = 9000
+outfile = 'data/data.mx'
+trainSize = 9000
 
 # criteoData = parser.ParseCriteoDataFile(filepath)
 # print criteoData['labels'].shape
 # print type(criteoData['labels'])
 # print criteoData['features'].shape
 # print type(criteoData['features'])
+
+parser.SaveCriteoDataAsNDArray(filepath,outfile)
 
 # X_train = criteoData['features'][0:trainSize,:]
 # X_test = criteoData['features'][trainSize:,:]
@@ -24,6 +25,6 @@ filepath = 'day_0_10000.txt'
 # print X_train.shape
 # print X_test.shape
 
-data_train = mx.io.CSVIter(data_csv="./data/data_3.csv", data_shape=(3,13,1),
-                           label_csv="./data/labels_3.csv", label_shape=(3,),
-                           batch_size=1)
+# data_train = mx.io.CSVIter(data_csv="./data/data_3.csv", data_shape=(13,),
+#                            label_csv="./data/labels_3.csv", label_shape=(3,),
+#                            batch_size=1)
--- example/find_mxnet.py
+++ example/find_mxnet.py
@@ -1,10 +1,7 @@
-try:
-	print "importing installed mxnet"
-	import mxnet as mx
-except ImportError:
-	print "importing mxnet in parent dir"
-	import os, sys
-	curr_path = os.path.abspath(os.path.dirname(__file__))
-	sys.path.append(os.path.join(curr_path, "../mxnet/python"))
-	sys.path.append(os.path.join(curr_path, "../mxnet"))
-	import mxnet as mx
+
+print "importing mxnet in parent dir"
+import os, sys
+curr_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(curr_path, "../mxnet/python"))
+sys.path.append(os.path.join(curr_path, "../mxnet"))
+import mxnet as mx
